43f64f3c4ada8d61efa0af3d5d961838a51e4ebd
jeremief <quasimobeau@yahoo.fr>
2016-10-09 12:31:33 +1100

    Feat: implement cancel game

--- game_logic_exploration.py
+++ game_logic_exploration.py
@@ -8,6 +8,11 @@ import random
 # class Player(object):
 #     """Player profile"""
 
+# class Score(object):
+#    """docstring for Score"""
+
+
+
 def display_current_game(current_game):
     display = ""
     for i in current_game:
@@ -26,6 +31,7 @@ strikes_left = 0
 user_input = ""
 user_input_valid = 0
 game_over = 0
+game_cancelled = 0
 won = 0
 answer = ""
 current_game = ""
@@ -60,44 +66,49 @@ answer = (list(answer))
 
 while current_game != answer and game_over !=1:
     display_current_game(current_game)
+    print " "
     print "%i strike(s) left" % strikes_left
     print " "
     user_input = raw_input("Please enter a letter:")
 
     # Validate user input
-    print "\n"
     user_input_valid = 1
+    user_input = user_input.upper()
     if user_input.isdigit() == True:
         print "Letters only please..."
         user_input_valid = 0
     if user_input.isalnum() == False:
         print "Letters only please..."
         user_input_valid = 0
-    if len(user_input) != 1:
+    if len(user_input) != 1 and user_input != "CANCEL":
         print "Single character please..."
         user_input_valid = 0
     print "\n"
 
     # Test user input against answer
     if user_input_valid == 1:
-        user_input = user_input.upper()
-        if user_input in answer:
-            for i in answer:
-                if user_input == i:
-                    for j in range(0,len(current_game)):
-                        if answer[j] == i:
-                            current_game[j]= user_input
-            print "CORRECT"
-            if current_game == answer:
-                won = 1
-                display_current_game(current_game)
+        if user_input != "CANCEL":
+            if user_input in answer:
+                for i in answer:
+                    if user_input == i:
+                        for j in range(0,len(current_game)):
+                            if answer[j] == i:
+                                current_game[j]= user_input
+                print "CORRECT"
+                if current_game == answer:
+                    won = 1
+                    game_over = 1
+                    display_current_game(current_game)
+            else:
+                print "WRONG"
+                strikes_left -= 1
+                print " "
+                if strikes_left == 0 :
+                    game_over = 1
+                    won = 0
         else:
-            print "WRONG"
-            strikes_left -= 1
-            print " "
-            if strikes_left == 0 :
-                game_over = 1
-                won = 0
+            game_cancelled = 1
+            game_over = 1
 
     
 
@@ -107,5 +118,7 @@ while current_game != answer and game_over !=1:
 
 if won == 1:
     print "YOU WON"       
-else:
+if won == 0 and game_cancelled == 0:
     print "YOU LOST"
+else:
+    print "GAME CANCELLED"
