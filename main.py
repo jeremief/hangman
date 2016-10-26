f0ee553a0c41be75a8fea2702bad215a18bb837b
jeremief <quasimobeau@yahoo.fr>
2016-10-26 18:35:46 +1100

    Feat: add cron job to send email to users with pending games. Not yet tested against live Google platform

--- main.py
+++ main.py
@@ -8,18 +8,27 @@ import webapp2
 from google.appengine.api import mail, app_identity
 from api import HangmanApi
 
-from models import User
+from models import User, Game
 
 
 class SendReminderEmail(webapp2.RequestHandler):
     def get(self):
         """Send a reminder email to each User with an email about games.
-        Called every hour using a cron job"""
+        Called every 24 hours using a cron job"""
         app_id = app_identity.get_application_id()
-        users = User.query(User.email != None)
+        games = Game.query(Game.game_over == False).fetch()
+        print "Games: " + str(games)
+        users = []
+        for game in games:
+            user = User.query(game.user == User.key).get()
+            if user not in users:
+                users.append(user)
+
+        print "Users: " + str(users)
+
         for user in users:
             subject = 'This is a reminder!'
-            body = 'Hello {}, try out Guess A Number!'.format(user.name)
+            body = 'Hello {}, try out Hangman!'.format(user.name)
             # This will send test emails, the arguments to send_mail are:
             # from, to, subject, body
             mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
@@ -27,15 +36,6 @@ class SendReminderEmail(webapp2.RequestHandler):
                            subject,
                            body)
 
-
-class UpdateAverageMovesRemaining(webapp2.RequestHandler):
-    def post(self):
-        """Update game listing announcement in memcache."""
-        GuessANumberApi._cache_average_attempts()
-        self.response.set_status(204)
-
-
 app = webapp2.WSGIApplication([
     ('/crons/send_reminder', SendReminderEmail),
-    ('/tasks/cache_average_attempts', UpdateAverageMovesRemaining),
 ], debug=True)
